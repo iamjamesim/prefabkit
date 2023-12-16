fbc7c7fe0f5680679d0516ea55e9b9d1bb4795c6
James Im <28934835+iamjamesim@users.noreply.github.com>
2023-12-15 20:18:20 -0800

    fix: Item card more button bugs (#7)
    * Replace item card actions context menu with a confirmation dialog
    
    * Hide item card more button when no actions are available

--- Sources/PrefabAppUI/Components/Cards/ItemCardMoreMenu.swift
+++ Sources/PrefabAppUI/Components/Cards/ItemCardMoreMenu.swift
@@ -7,24 +7,33 @@ struct ItemCardMoreMenu: View {
     /// A closure that is called when a menu item is tapped.
     let onMenuItemTap: (ItemCardMenuItem.Action) -> Void
 
+    @State private var isPresented: Bool = false
+
     var body: some View {
-        Menu {
-            ForEach(menuItems, id: \.action) { menuItem in
-                switch menuItem.action {
-                case .delete:
-                    Button(role: .destructive) {
-                        onMenuItemTap(menuItem.action)
-                    } label: {
-                        Label("Delete", systemImage: "trash")
-                    }
-                case .unknown:
-                    EmptyView()
-                }
+        if menuItems.isEmpty {
+            EmptyView()
+        } else {
+            Button {
+                isPresented = true
+            } label: {
+                Image(systemName: "ellipsis")
+                    .font(.system(size: 16, weight: .medium))
+                    .frame(width: 32, height: 32)
             }
-        } label: {
-            Image(systemName: "ellipsis")
-                .font(.system(size: 16, weight: .medium))
-                .frame(width: 32, height: 32)
+            .confirmationDialog("Item Actions", isPresented: $isPresented, titleVisibility: .hidden, actions: {
+                ForEach(menuItems, id: \.action) { menuItem in
+                    switch menuItem.action {
+                    case .delete:
+                        Button(role: .destructive) {
+                            onMenuItemTap(menuItem.action)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    case .unknown:
+                        EmptyView()
+                    }
+                }
+            })
         }
     }
 }
